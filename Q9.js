30a90a51ac4fffd69ec3d77594bf851515cebedc
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:27:47 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,4 @@
+"use strict";
+//9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favoritNum = (4);
+console.log(`number ${favoritNum} is my favorite number`);
---
+++ Q9.ts
@@ -0,0 +1,4 @@
+//9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoritNum:number=(4)
+console.log (`number ${favoritNum} is my favorite number`)
